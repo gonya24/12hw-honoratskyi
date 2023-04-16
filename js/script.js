3178562627836479ae075c001f34f99214734de0
Illia <illia.gonya@icloud.com>
2023-04-16 17:12:19 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function removeElement (a,b) {
-   let index = a.indexOf(b)
-    return a.splice(index,1)
+   let index = a.indexOf(b);
+    return a.splice(index,1);
 }
 const array = [1, 2, 3, 4, 5, 6, 7];
 removeElement(array, 5);
